968aefd0bd92af31004fefc3aa764ea18ca9a2a5
Nikoletta <nikolettaliubka@gmail.com>
2025-09-17 21:35:01 +0300

    fix api

--- frontend/src/shared/api/api.ts
+++ frontend/src/shared/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://flower-backend.onrender.com",
+    baseURL: "https://flower-delivery-app-w67d.onrender.com",
 })
